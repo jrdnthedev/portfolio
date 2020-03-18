0913ccc1d66f7af4f9d18ddc140ed6e907f0109a
andre mcmorris <andre.mcmorris@bmo.com>
2020-03-18 19:16:06 -0400

    stafrting changes to page structure/design

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 'use strict';
 
 (function(){
-  console.log("...running");
-
   var menu =  document.getElementById('hamburger-menu');
   var navigation =  document.getElementById('navigation');
-
   menu.addEventListener('click',function(){
-
-    console.log('clicked');
-
     if(navigation.classList.contains('show')){
       navigation.classList.remove('show');
     }else{
       navigation.classList.add('show');
     }
   });
+
+  var getIntroHeight = function () {
+    const height = window.innerHeight - 72;
+    setIntroheight(height);
+  }
+
+  var setIntroheight = function (height) {
+    var section = document.getElementsByClassName('header__hero');
+    section[0].style.height = height + 'px';
+    console.log(section[0]);
+  }
+
+  getIntroHeight();
+  window.addEventListener('resize', getIntroHeight);
 }());
